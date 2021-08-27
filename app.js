edb2cb50a380a626942138671fe516dada4f29fe
97baek <170410_@naver.com>
2021-08-27 20:53:22 +0900

    [UPDATE] hashchange 이벤트 리스너 추가
    - 게시글의 제목을 클릭하면 화면에 제목이 나옵니다.

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const ajax = new XMLHttpRequest();
+
+const container = document.getElementById("root");
+const $content = document.createElement("div");
+const $ul = document.createElement("ul");
+
+const newsFeed = JSON.parse(ajax.response);
+
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = `https://api.hnpwa.com/v0/item/@id.json`;
 
 ajax.open("GET", NEWS_URL, false);
 ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
-const $ul = document.createElement("ul");
+window.addEventListener("hashchange", () => {
+  const id = location.hash.substr(1);
+
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const $title = document.createElement("h1");
+
+  $title.innerHTML = newsContent.title;
+
+  $content.appendChild($title);
+});
 
 newsFeed.forEach((feed) => {
   const $div = document.createElement("div");
@@ -21,4 +40,5 @@ newsFeed.forEach((feed) => {
   $ul.appendChild($div.firstElementChild);
 });
 
-document.getElementById("root").appendChild($ul);
+container.appendChild($ul);
+container.appendChild($content);
