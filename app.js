87d970c198c8b21acc6466379825d583216c17ee
97baek <170410_@naver.com>
2021-08-27 21:27:57 +0900

    [UPDATE] 라우팅 기능 구현
    - 기존에 newsFeed 렌더링을 담당하던 부분을 함수로 묶었습니다.
    - router() 함수에서 url에 따라 어떤 것을 렌더링할지 판단합니다.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const ajax = new XMLHttpRequest();
 
 const container = document.getElementById("root");
-const $content = document.createElement("div");
 const $ul = document.createElement("ul");
 
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
@@ -14,33 +13,46 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
+function renderNewsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener("hashchange", () => {
+  newsList.push("<ul>");
+  newsFeed.forEach((feed) => {
+    newsList.push(`
+      <li>
+        <a href="#${feed.id}">
+          ${feed.title} (${feed.comments_count})
+        </a>
+      </li>
+    `);
+  });
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
+}
+
+function renderNewsDetail() {
   const id = location.hash.substr(1);
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
   container.innerHTML = `
-    <h1>${newsContent.title}</h1>
-    <div>
-      <a href="#">목록</a>
-    </div>
-  `;
-});
+      <h1>${newsContent.title}</h1>
+      <div>
+        <a href="#">목록</a>
+      </div>
+    `;
+}
 
-newsFeed.forEach((feed) => {
-  const $div = document.createElement("div");
+function router() {
+  const routePath = location.hash;
+  if (!routePath) {
+    renderNewsFeed();
+    return;
+  }
+  renderNewsDetail();
+}
 
-  $div.innerHTML = `
-    <li>
-      <a href="#${feed.id}">
-        ${feed.title} (${feed.comments_count})
-      </a>
-    </li>
-  `;
+window.addEventListener("hashchange", router);
 
-  $ul.appendChild($div.firstElementChild);
-});
-
-container.appendChild($ul);
-container.appendChild($content);
+router();
