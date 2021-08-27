5ca0df4ff01194d9b4afe2290e9cf92c7585a8d5
97baek <170410_@naver.com>
2021-08-27 20:37:28 +0900

    [UPDATE] JS로 DOM 요소 추가
    - 자바스크립트를 이용해 DOM 요소를 추가했습니다.

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 
 ajax.open("GET", NEWS_URL, false);
 ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+const $ul = document.createElement("ul");
+
+newsFeed.forEach((feed) => {
+  const $div = document.createElement("div");
+
+  $div.innerHTML = `
+    <li>
+      <a href="#${feed.id}">
+        ${feed.title} (${feed.comments_count})
+      </a>
+    </li>
+  `;
+
+  $ul.appendChild($div.firstElementChild);
+});
+
+document.getElementById("root").appendChild($ul);
