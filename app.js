218b536a80b2458e8fedf4cabf0de92dda64fd9a
97baek <170410_@naver.com>
2021-08-29 17:42:48 +0900

    [UPDATE] 페이징 기능 추가
    - 다음 페이지, 이전 페이지로 페이지 이동 기능을 추가했습니다.
    - 공통적으로 사용할 변수들을 store 객체 안에 담기 위해 생성했습니다.

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const $ul = document.createElement("ul");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = `https://api.hnpwa.com/v0/item/@id.json`;
 
+const store = {
+  currentPage: 1,
+};
+
 function getData(url) {
   ajax.open("GET", url, false);
   ajax.send();
@@ -14,39 +18,52 @@ function getData(url) {
 }
 
 function renderNewsFeed() {
+  console.log(store.currentPage);
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
+  console.log(newsFeed);
 
   newsList.push("<ul>");
-  newsFeed.forEach((feed) => {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
       <li>
-        <a href="#${feed.id}">
-          ${feed.title} (${feed.comments_count})
+        <a href="#/show/${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
       </li>
     `);
-  });
-  newsList.push("</ul>");
+  }
 
+  newsList.push("</ul>");
+  newsList.push(`
+    <div>
+      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+      <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+    </div>
+  `);
   container.innerHTML = newsList.join("");
 }
 
 function renderNewsDetail() {
-  const id = location.hash.substr(1);
+  const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
   container.innerHTML = `
-      <h1>${newsContent.title}</h1>
-      <div>
-        <a href="#">목록</a>
-      </div>
-    `;
+    <h1>${newsContent.title}</h1>
+    <div>
+      <a href="#/page/${store.currentPage}">목록</a>
+    </div>
+  `;
 }
 
 function router() {
   const routePath = location.hash;
   if (!routePath) {
+    renderNewsFeed();
+    return;
+  }
+  if (routePath.includes("#/page/")) {
+    store.currentPage = parseInt(routePath.substr(7));
     renderNewsFeed();
     return;
   }
